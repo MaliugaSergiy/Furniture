35f2e6f67208c08cddc31dcafd6a676fbe6f964a
MaliugaSergiy <sergii.maliuga@gmail.com>
2017-08-03 12:53:00 +0300

    разнесено по темплейтам и настроено верхнее меню

--- dev/js/common.js
+++ dev/js/common.js
@@ -1,7 +1,47 @@
+	$(document).ready(function() {
+
+		$("#my-menu").mmenu({
+			"extensions": [
+				"pagedim-white",
+				"fx-menu-fade",
+				//"listview-justify",
+				"multiline",
+				"shadow-page",
+				//"popup"
+			],
+			"offCanvas": {
+				"position": "left",
+				//"zposition": "front"
+			},
+			"counters": true,
+			"navbars": [{
+					"position": "top",
+					"content": [
+						"searchfield"
+					]
+				},
+				{
+					"position": "top"
+				},
+				{
+					"position": "bottom",
+					"content": [
+						"<a class='fa fa-envelope' href='#/'></a>",
+						"<a class='fa fa-twitter' href='#/'></a>",
+						"<a class='fa fa-facebook' href='#/'></a>"
+					]
+				}
+			]
+
+		});
+
+		var API = $("#my-menu").data("mmenu");
+		$(window).on("resize", function() {
+			API.close();
+		});
 
 
-$(document).ready(function () {
+		$('[data-toggle="tooltip"]').tooltip();
 
 
-
-});
+	})
